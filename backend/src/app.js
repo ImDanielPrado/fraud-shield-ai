cf5c70053d7a8c4c4064417ed0ed9c6e5216e9d2
Daniel Prado <bussinesprado@gmail.com>
2026-06-27 09:05:03 -0400

    feat: small adjust in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,23 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
 const transactionRoutes = require("./routes/transactionRoutes");
-const alertRoutes = require("./routes/alertRoutes");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+  origin: "http://localhost:3001",
+  credentials: true
+}));
 
+app.use(express.json());
+
+// rotas
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/transactions", transactionRoutes);
-app.use("/api/alerts", alertRoutes);
 
 module.exports = app;
--- backend/src/controllers/AuthController.js
+++ backend/src/controllers/AuthController.js
@@ -1,4 +1,4 @@
-const authService = require("../services/AuthService");
+ const authService = require("../services/AuthService");
 
 class AuthController {
   async login(req, res) {
